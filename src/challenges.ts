9073b66ea5f0abf5d70c621a9975e7666d44264b
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-15 00:31:05 -0700

    added first challenge card details

---
+++ src/challenges.ts
@@ -0,0 +1,12 @@
+export const challenges = [
+  {
+    id: 1,
+    sitelink:
+      "https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV",
+    href: "/results-summary-comp",
+    title: "Results Summary Component",
+    body: "A component that displays a summary of the results of a test.",
+    status: "In-progress",
+    tags: ["html", "css", "tailwindcss"],
+  },
+];
