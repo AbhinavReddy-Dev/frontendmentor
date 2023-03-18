b636c6d4aaddd957d51fae61adaf0291c9d7a27b
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-17 20:32:49 -0700

    added date to challenge card

--- src/challenges.ts
+++ src/challenges.ts
@@ -1,6 +1,6 @@
 export const challenges = [
   {
-    id: 1,
+    id: "1",
     sitelink:
       "https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV",
     href: "/results-summary-comp",
@@ -8,9 +8,10 @@ export const challenges = [
     body: "A component that displays a summary of the results of a test.",
     status: "Finished",
     tags: ["html", "css", "Astro", "tailwindcss"],
+    date: "03-15-2023",
   },
   {
-    id: 2,
+    id: "2",
     sitelink:
       "https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa/hub",
     href: "/product-review-card",
@@ -18,5 +19,6 @@ export const challenges = [
     body: "A card that displays the details of a perfume.",
     status: "In-progress",
     tags: ["html", "css", "Astro", "tailwindcss"],
+    date: "03-17-2023",
   },
 ];
